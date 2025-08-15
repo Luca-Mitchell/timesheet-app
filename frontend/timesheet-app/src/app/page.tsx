54937eb4e5ad8ec1324b81a746cc513fcb2e6a8d
Luca Mitchell <lucajmitchell@gmail.com>
2025-08-15 18:28:52 +0100

    Move login form ui code to main page.tsx file from seperate component file.

--- frontend/timesheet-app/src/app/page.tsx
+++ frontend/timesheet-app/src/app/page.tsx
@@ -1,11 +1,57 @@
-import { LoginForm } from "@/components/login-form"
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <div className='flex flex-col gap-6'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Login or sign up</CardTitle>
+              <CardDescription>
+                If you dont have an account, enter your email and password and
+                click "Sign up" to create one.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="email@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" type="password" required />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                    <Button variant="outline" className="w-full">
+                      Sign up
+                    </Button>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
-  )
+  );
 }
